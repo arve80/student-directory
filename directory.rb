957d89c45c2c8cf45ea89b224ffe579957da1ec4
Arve80 <alvaltor@gmail.com>
2014-08-07 16:49:10 +0100

    version_7 refactor

--- directory.rb
+++ directory.rb
@@ -1,3 +1,123 @@
+
+@students = []
+
+$col_0 = "\e[0;31m" 
+$col_1 = "\e[0m"
+
+
+
+def add_student(name,cohort)
+		
+	@students << {name: name, cohort: cohort}
+
+end
+
+def show_students
+	print_header
+	print_student(@students)
+	print_footer(@students)	
+end
+
+def input_students
+	
+	puts "Please enter the names of the students and cohort separated by \"comma\""
+	puts "To finish, just hit return twice "
+
+	#get the first name
+	name = gets.chomp
+
+		#while the name is not empty, repeat this code
+	while !name.empty? do
+		
+		add_student(name, :August)
+
+		puts "Now we have #{@students.length} students "
+
+		#get another name from the user
+		puts "Enter another name"
+		name = gets.chomp
+
+	end
+
+	#return students array
+	@students
+
+end
+
+def print_header
+
+	puts	"The students of my cohort at Makers Academy"
+	puts	"==========================================="
+end
+
+def print_student(students)
+
+	stud_length = students.length
+
+	count = 0
+
+	while count < stud_length 
+
+		puts "#{$col_0}Name:#{$col_1} #{students[count][:name].center(12)}"
+		puts "#{$col_0}Cohort:#{$col_1} #{students[count][:cohort].to_s.center(8)}"
+		puts "==================================================="
+
+		count +=1
+	end
+
+=begin	
+
+	students.each_with_index do |(key),index| # Add 'value' inside parenthesis if you wanna access it.
+
+		puts "#{$color[0]}#{index+1}#{$color[1]} #{key[:name]} (#{key[:cohort]} cohort) "
+	
+	end
+=end
+
+end
+
+def print_footer(students)
+
+	puts "Overall we have #{students.length} students" 	
+
+end
+
+
+def interactive_menu
+
+	loop do
+
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+
+		selection = gets.chomp
+
+		case selection
+
+			when '1'
+					@students = input_students
+
+			when '2'
+				show_students
+			when '9'
+				exit
+			else
+				puts "I don't know what you meant, try again"
+			
+		end
+	end
+end
+
+interactive_menu
+
+#students = input_students
+
+#print_header
+#print_student(students)
+#print_footer(students)
+
+
 =begin
 students = [
 				{ name: "Ruth Earle", cohort: :august},
@@ -161,123 +281,3 @@ arg_to_search = gets.chomp
 arg_to_search == 'l' ? search_by_letter(students) : search_by_count(students)
 
 =end
-
-$col_0 = "\e[0;31m" 
-$col_1 = "\e[0m"
-
-def input_students(cohort='August')
-	
-	puts "Please enter the names of the students and cohort separated by \"comma\""
-	puts "To finish, just hit return twice "
-
-	#create and empty array
-	students = []
-
-	#get the first name
-	name = gets.chomp
-
-	
-
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		
-		cohort = gets.chomp
-
-		puts cohort
-
-		#get_user_details = set_user_details
-
-		students << {name: name,cohort: cohort}
-
-		puts "Now we have #{students.length} students "
-
-		#get another name from the user
-		puts "Enter another name"
-		name = gets.chomp
-
-	end
-
-	#return students array
-		students
-
-end
-
-def print_header
-
-	puts	"The students of my cohort at Makers Academy"
-	puts	"==========================================="
-end
-
-def print_student(students)
-
-	stud_length = students.length
-
-	count = 0
-
-	while count < stud_length 
-
-		puts "#{$col_0}Name:#{$col_1} #{students[count][:name].center(25)}"
-		puts "#{$col_0}Cohort:#{$col_1} #{students[count][:cohort]}"
-		puts "==================================================="
-
-		count +=1
-	end
-
-=begin	
-
-	students.each_with_index do |(key),index| # Add 'value' inside parenthesis if you wanna access it.
-
-		puts "#{$color[0]}#{index+1}#{$color[1]} #{key[:name]} (#{key[:cohort]} cohort) "
-	
-	end
-=end
-
-end
-
-def print_footer(students)
-
-	puts "Overall we have #{students.length} students" 	
-
-end
-
-
-def interactive_menu
-
-	students = []
-
-	loop do
-
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-
-		selection = gets.chomp
-
-		case selection
-
-			when '1'
-					students = input_students
-
-			when '2'
-					print_header
-					print_student(students)
-					print_footer(students)				
-
-			when '9'
-				exit
-			else
-				puts "I don't know what you meant, try again"
-			
-		end
-	end
-end
-
-interactive_menu
-
-#students = input_students
-
-#print_header
-#print_student(students)
-#print_footer(students)
-
-
